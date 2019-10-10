64fa91c24a3314338c47da4436c029e8e4eae764
pablozermenoga <pzg97@hotmail.com>
2019-10-10 08:52:45 -0600

    ngfor en slides y creacion de objetos gallery

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  slideOpts={
+    initialSlide:0,
+    speed:200
+  };
+
+  gallery=[
+    {
+      img:"http://yosoydenayarit.com/wp-content/uploads/2015/09/Plaza-de-Armas-en-Tepic-Nayarit-620x400.jpg  ",
+      subtitle:"Destino",
+      title:"Tepic, Nayarit",
+      content:"Fundada en 1532, en tepic"
+    },
+    {
+      img:"http://a.uan.mx/im/540,290,1,90,1,i/tepic_1.jpg",
+      subtitle:"Destino",
+      title:"Tepic, Nayarit",
+      content:"Fundada en 1532, en tepic"
+    },  
+
+  ]
+
+  
+
+
   constructor() {}
 
 }
